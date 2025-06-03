f47915699aa38f71957ff4bcba6094222af8fdf0
Tracy <traciinguyen@hotmail.com>
2025-06-03 13:31:41 -0400

    Update timer to have start and reset buttons

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -3,25 +3,35 @@ import { useEffect, useRef, useState } from "react";
 const Timer = ({ timeLimit = 60 * 1000 }: { timeLimit?: number }) => {
     const requestId = useRef<number>(undefined);
     const [timeRemaining, setTimeRemaining] = useState<number>(timeLimit / 1000);
+    const startTime = useRef<number>(-1);
 
-    useEffect(() => {
-        updateTimeRemaining();
-        return () => cancelAnimationFrame(requestId.current || 0);
-    });
-
-    const updateTimeRemaining = () => {
-        if (timeRemaining <= 0) {
-            cancelAnimationFrame(requestId.current || 0);
-            return;
-        }
-
-        setTimeRemaining(Math.floor((timeLimit - performance.now()) / 1000));
+    function updateTimeRemaining() {
+        setTimeRemaining(Math.floor((timeLimit - (performance.now() - startTime.current)) / 1000));
         requestId.current = requestAnimationFrame(updateTimeRemaining);
     };
 
+    const startTimer = () => {
+        cancelAnimationFrame(requestId.current || 0);
+        setTimeRemaining(timeLimit / 1000);
+        startTime.current = performance.now();
+        requestId.current = requestAnimationFrame(updateTimeRemaining);
+    };
+
+    const resetTimer = () => {
+        cancelAnimationFrame(requestId.current || 0);
+        setTimeRemaining(timeLimit / 1000);
+    };
+
+    useEffect(() => {
+        if (timeRemaining <= 0) {
+            cancelAnimationFrame(requestId.current || 0);
+        }
+    });
+
     return (
         <div>
-            <button onClick={updateTimeRemaining}>Click</button>
+            <button onClick={startTimer}>Start</button>
+            <button onClick={resetTimer}>Reset</button>
             <p>Time: {timeRemaining}</p>
         </div>
     );
